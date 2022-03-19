bd7ba7743c93df2bae5596bc10ac3bee81b476e5
Nadya Filimonova <filim0nova.nadezhda@yandex.ru>
2022-03-19 23:16:44 +0500

    на проверку 2

--- backend/app.js
+++ backend/app.js
@@ -17,7 +17,7 @@ const { validateLogin } = require('./middlewares/validation');
 const NotFoundError = require('./errors/not-found-error');
 const { requestLogger, errorLogger } = require('./middlewares/logger');
 
-const { PORT = 3001 } = process.env;
+const { PORT = 3000 } = process.env;
 
 const app = express();
 
